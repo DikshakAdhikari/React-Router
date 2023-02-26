1d2f7802f8c60225951c3a9568953fbbeee99875
DikshakAdhikari <ddikshakk@gmail.com>
2023-02-27 03:39:03 +0530

    How to lazy load the About page - 14

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom'
-import { About } from './components/About';
+// import { About } from './components/About';
 import { FeaturedProducts } from './components/FeaturedProducts';
 import { NewProducts } from './components/NewProducts';
 import { Home } from './components/Home';
@@ -10,6 +11,7 @@ import { Products } from './components/Products';
 import { Users } from './components/Users';
 import { UserDetails } from './components/UserDetails';
 import { Admin } from './components/Admin';
+const LazyAbout= React.lazy(()=> import('./components/About'))
 
 
 
@@ -19,7 +21,13 @@ function App() {
     <Navbar />
    <Routes>
     <Route path='/' element= {<Home />}></Route>
-    <Route path='about' element={<About />}></Route>
+    <Route 
+    path='about' 
+    element={
+     <React.Suspense fallback='Loading...'>
+       <LazyAbout />
+     </React.Suspense>}>  
+    </Route>
     <Route path='order-summary' element= {<OrderSummary />} />
   
     <Route path='products' element={<Products />} > 
