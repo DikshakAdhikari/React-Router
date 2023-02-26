60f7bad4a67e30cafb5f9af929e4d9ed14eb0be2
DikshakAdhikari <ddikshakk@gmail.com>
2023-02-27 02:31:12 +0530

    used useParams to get users id dynamically - 11

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export const UserDetails = () => {
+    const params = useParams() //The hook returns an object of key value pairs
+    const userId = params.userId //inside params object use useId variable
   return (
-    <div>Details about user</div>
+    <div>Details about user {userId}</div>
   )
 }
